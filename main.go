399d85de48f07d0c76c95785483610df54726530
Craig Iverson <civerson@google.com>
2022-03-22 22:33:40 +0000

    Now replies Welcome, from x

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "%v", r.Header.Get("provider"))
+		fmt.Fprintf(w, "Welcome, from %v\n", r.Header.Get("provider"))
 	})
 	log.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
